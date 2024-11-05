f6686a6875fd8aa3eb59bd57d7d366d5eb193867
Tristan <55209910+tristanjtate@users.noreply.github.com>
2024-11-04 18:59:03 -0700

    utilized tailwind for most of design, left off on needing to make responsove for smaller screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "my-orange": "hsl(25, 97%, 53%)",
+        "very-dark-blue": "hsl(216, 12%, 8%)", // Use kebab-case for naming
+        "my-light-grey": "hsl(217, 12%, 63%)",
+        "my-dark-blue": "hsl(213, 19%, 18%)",
+      },
+      fontFamily: {
+        overpass: ["Overpass", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
